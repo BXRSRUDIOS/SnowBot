efa44154980904934e341d96b67eac3304928565
Bilal Raja <bilalraja@Bilals-iMac.local>
2020-12-19 17:18:14 +0000

    2nd Commit: Test to see if  Issue With node_modules was fixed

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const DiscordJS = require('discord.js')
 const WOKCommands = require('wokcommands')
 require('dotenv').config()
- 
-const client = new DiscordJS.Client()
- 
+
+const client = new DiscordJS.Client({
+  partials: ['MESSAGE', 'REACTION'],
+})
+
 client.on('ready', () => {
   // Initialize WOKCommands
-  console.log("Ready!!")
   new WOKCommands(client)
   .setMongoPath(process.env.MONGO_URI)
   .setDefaultPrefix(process.env.DEFAULT_PREFIX)
   .setSyntaxError('Incorrect syntax! Please use {PREFIX}{COMMAND} {ARGUMENTS}')
 })
- 
+
 client.login(process.env.TOKEN)
